89c532cd8f919d40aabca1d4523650cc700469ec
Deepanshu Tewari <tewarideepanshu804@gmail.com>
2026-03-31 23:07:30 +0530

    TRAINER ,MODEL EVALUATION AND MLFLOW

---
+++ datataaaaaaaaaaaaaaaaaaaaaaaaaaaa.py
@@ -0,0 +1,3 @@
+# os.environ["MLFLOW_TRACKING_URL"]="https://dagshub.com/tewarideepanshu804/NETWORK_SECURITY_SYSTEM.mlflow"
+# os.environ["MLFLOW_TRACKING_USERNAME"]="tewarideepanshu804"
+# os.environ["MLFLOW_TRACKING_PASSWORD"]="20cb7824348f946260ee76e7ac45cae9ccfb522b"
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+print(datetime.now())
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
@@ -6,8 +8,8 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
-# from networksecurity.components.model_trainer import ModelTrainer
-# from networksecurity.entity.config_entity import ModelTrainerConfig
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
  
 
 import sys
@@ -39,14 +41,15 @@ if __name__=='__main__':
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
 
-        # logging.info("Model Training sstared")
-        # model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
-        # model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
-        # model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
 
-        # logging.info("Model Training artifact created")
+        logging.info("Model Training artifact created")
         
         
         
     except Exception as e:
            raise NetworkSecurityException(e,sys)
+print(datetime.now())
